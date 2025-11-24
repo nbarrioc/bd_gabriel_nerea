2b73838ef079c45520edfeed5f308dc7c4d4d4d9
nbarrioc <nbarrioc@gmail.com>
2025-11-24 23:37:04 +0100

    Creo el archivo config_data con los datos de acceso del usuario

---
+++ trabajo_bd/config_data.py
@@ -0,0 +1,9 @@
+# %%
+# Sensitive information the user will use to acces the database
+config = {
+    'host' :'localhost',
+    'port' : 3306,
+    'user' : 'biotec',
+    'password' : 'biotec_pwd',
+    'database' : 'disnet_drugslayer' 
+}
