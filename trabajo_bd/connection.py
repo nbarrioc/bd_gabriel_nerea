ea6663d8de5049564d3dbaa60fb7a013266633b0
nbarrioc <nbarrioc@gmail.com>
2025-11-24 23:56:44 +0100

    Había un error de sintaxis en la conexión

--- trabajo_bd/connection.py
+++ trabajo_bd/connection.py
@@ -4,4 +4,4 @@ import mysql.connector
 import config_data
 
 def get_connection():
-    return mysql.connection.connect(**config_data.config)
+    return mysql.connector.connect(**config_data.config)
