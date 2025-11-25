8b79b651fad792d4737011c2bdecb8eac7dd4a0d
nbarrioc <nbarrioc@gmail.com>
2025-11-25 22:53:13 +0100

    Ya están creadas las funciones de los apartados 5, 6 y 7

--- trabajo_bd/queries.py
+++ trabajo_bd/queries.py
@@ -59,7 +59,12 @@ def query_5b(conn):
 # %%
 # Modification queries
 
-query_6a = """
+# 6. Deletes
+
+# 6a. List of interactions with lowest inferred score
+def query_6a(conn):
+    cursor = conn.cursor()
+    query1 = """
            SELECT inferred_score, drug_name, disease_name
            FROM drug_disease, drug, disease_has_code, disease
            WHERE inferred_score IS NOT NULL AND
@@ -69,26 +74,81 @@ query_6a = """
            ORDER BY inferred_score, drug_name, disease_name ASC
            LIMIT 10;
            """
+    
+    cursor.execute(query1)
 
-query_6b = """
-           DELETE FROM drug_disease
-           WHERE inferred_score=%s AND
-           drug_id =
-                (SELECT drug_id
-                FROM drug
-                WHERE drug_name="%s") AND
-           code_id =
-            	(SELECT code_id
-	            FROM disease_has_code, disease
-	            WHERE disease_name="%s" AND
-		            disease_has_code.disease_id=disease.disease_id);"""
+    data1 = cursor.fetchall()
 
-query_7 = """
-          INSERT INTO drug_has_code
-          VALUES (
-	          (SELECT drug_id
-	          FROM drug
-	          WHERE drug_name = "%s"),
-	          %s,
-	          "%s");
+    i = 0
+    interactions = []
+
+    for row in data1:
+        interactions[i] = row[0]
+        i = i+1
+
+    query2 = """
+             DELETE FROM drug_disease
+             WHERE inferred_score=%s AND
+             drug_id =
+                  (SELECT drug_id
+                  FROM drug
+                  WHERE drug_name=%s) AND
+             code_id =
+            	  (SELECT code_id
+	              FROM disease_has_code, disease
+	              WHERE disease_name=%s AND
+		              disease_has_code.disease_id=disease.disease_id);
+             """ 
+    
+    data2 = cursor.fetchall()
+
+    j = 1
+
+    for row in data2:
+        print("[",j,"]. ", row[0])
+        j = j+1
+    
+    option_number = int(input("Por favor, introduzca el número de la fila que desea eliminar: "))
+
+    inferred_score_selected = interactions[option_number-1][0]
+    drug_name_selected = interactions[option_number-1][1]
+    disease_name_selected = interactions[option_number-1][2]
+
+    cursor.execute(query2,(inferred_score_selected,drug_name_selected,disease_name_selected,))
+
+    # Aquí habría que poner el conn.commit() si no estuviese el autocommit
+
+    print('Número de filas eliminadas: %s' % (cursor.rowcount))
+
+    cursor.close()
+
+
+# %%
+# 7. Insertions
+def query_7(conn):
+    cursor = conn.cursor()
+    query = """
+            INSERT INTO drug_has_code
+            VALUES (
+	            (SELECT drug_id
+	            FROM drug
+	            WHERE drug_name = "%s"),
+	            %s,
+	            %s);
           """
+    
+    print("Va a proceder a crear una nueva entrada en la base de datos con la información de codificación de un fármaco ya existente.")
+    print("Debe introducir los siguientes valores en el siguiente orden: nombre del fármaco, nuevo identificador del fármaco y nombre del vocabulario")
+    list_drug_code_values = input("Por favor, introduzca los valores: ")
+    
+    drug_name = list_drug_code_values[0]
+    drug_code = list_drug_code_values[1]
+    vocabulary = list_drug_code_values[2]
+    
+    cursor.execute(query,(drug_name,drug_code,vocabulary,))
+
+    print('Número de filas insertadas: %s' % (cursor.rowcount))
+
+    cursor.close()
+
+# %%
