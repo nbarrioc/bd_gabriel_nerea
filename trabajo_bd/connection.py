cdf7ef75204707cc8ec2387dd6925103337b7a4b
gabrielcollr <rieffel.gabriel@gmail.com>
2025-12-15 03:55:53 +0100

    nuevas versiones

---
+++ trabajo_bd/connection.py
@@ -0,0 +1,17 @@
+# %%
+# Connection stablishment
+import mysql.connector
+import config_data
+
+def get_connection():
+    conn = mysql.connector.connect(
+        user=config_data.config["user"],
+        password=config_data.config["password"],
+        host=config_data.config["host"],
+        database=config_data.config["database"],
+        use_pure=True
+    )
+    conn.autocommit = True
+    return conn
+
+# %%
